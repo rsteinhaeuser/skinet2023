887fef15d0158764d1747c6d2139732dc1f1bd35
René Steinhäuser <rene.steinhaeuser@gmx.de>
2023-01-14 15:39:21 +0100

    end of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -23,6 +23,11 @@ namespace API
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddSwaggerDocumentation();
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy =>{
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +45,7 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -10,20 +10,47 @@ namespace Core.Specifications
     {
         public BaseSpecification()
         {
-            
+
         }
-        
+
         public BaseSpecification(Expression<Func<T, bool>> criteria)
         {
             Criteria = criteria;
         }
 
-        public Expression<Func<T, bool>> Criteria {get;}
+        public Expression<Func<T, bool>> Criteria { get; }
 
-        public List<Expression<Func<T, object>>> Includes {get;} = new List<Expression<Func<T, object>>>();
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
 
-        protected void AddInclude(Expression<Func<T, object>> expression){
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
+        public int Take { get; private set; }
+
+        public int Skip { get; private set; }
+
+        public bool IsPagingEnabled { get; private set; }
+
+        protected void AddInclude(Expression<Func<T, object>> expression)
+        {
             Includes.Add(expression);
+        }
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderBy)
+        {
+            OrderBy = orderBy;
+        }
+
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescending)
+        {
+            OrderByDescending = orderByDescending;
+        }
+
+        protected void ApplyPaging(int skip, int take){
+            Skip = skip;
+            Take = take;
+            IsPagingEnabled = true;
         }
     }
 }
